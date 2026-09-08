e09971e3645c07b4fd66283316249da17fa3d57a
Yadala Bhargav <bhargavyadavyadala@gmail.com>
2026-09-08 12:33:33 +0530

    jjj

--- server/server.js
+++ server/server.js
@@ -1,16 +1,16 @@
 console.log("***** NEW SERVER.JS IS RUNNING *****");
 
-// const express = require("express");
-// const cors = require("cors");
-// const db = require("./db");
-// const jwt = require("jsonwebtoken");
+const express = require("express");
+const cors = require("cors");
+const db = require("./db");
+const jwt = require("jsonwebtoken");
 
-// const app = express();
+const app = express();
 
 const JWT_SECRET = "jobportal_secret_key";
 
-app.use(cors());
-app.use(express.json());
+// app.use(cors());
+// app.use(express.json());
 
 // JWT Verification Middleware
 const verifyToken = (req, res, next) => {
